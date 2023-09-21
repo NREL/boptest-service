bacfcec02bc0901c06a760ec989e88aa4629c37b
Kyle Benne <Kyle.Benne@nrel.gov>
2023-09-21 12:37:41 -0500

    Update test_bestest_air for Service compaitibility

--- boptest/testing/test_bestest_air.py
+++ boptest/testing/test_bestest_air.py
@@ -70,20 +70,20 @@ class Run(unittest.TestCase, utilities.partialTestTimePeriod):
 
         length = 86400*14
         # Get current step
-        step_current = requests.get('{0}/step'.format(self.url)).json()['payload']
+        step_current = requests.get('{0}/step/{1}'.format(self.url, self.testid)).json()['payload']
         # Initialize test case scenario
-        requests.put('{0}/scenario'.format(self.url), json={'time_period':'peak_heat_day'})
+        requests.put('{0}/scenario/{1}'.format(self.url, self.testid), json={'time_period':'peak_heat_day'})
         # Set simulation step
-        requests.put('{0}/step'.format(self.url), json={'step':length})
+        requests.put('{0}/step/{1}'.format(self.url, self.testid), json={'step':length})
         # Simulation Loop
-        requests.post('{0}/advance'.format(self.url), json=dict())
+        requests.post('{0}/advance/{1}'.format(self.url, self.testid), json=dict())
         # Try submit results to dashboard
-        status = requests.post("{0}/submit".format(self.url), json={"api_key": 'valid_key',
+        status = requests.post("{0}/submit/{1}".format(self.url, self.testid), json={"api_key": 'valid_key',
                                                                      "unit_test":"True"}).json()['status']
         # Check result
         self.assertEqual(status,200)
         # Return scenario and step to original
-        requests.put('{0}/step'.format(self.url), json={'step':step_current})
+        requests.put('{0}/step/{1}'.format(self.url, self.testid), json={'step':step_current})
 
 class API(unittest.TestCase, utilities.partialTestAPI):
     '''Tests the api for testcase.
